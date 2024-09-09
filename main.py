cc73649efc17bcc1d5e38a5991a9bf785d5eaadf
julianaMarin12 <142064728+julianaMarin12@users.noreply.github.com>
2024-09-09 16:32:23 -0500

    Funtions

---
+++ main.py
@@ -0,0 +1,13 @@
+"""Suma dos números"""
+def suma(num1, num2):#+
+    """
+    This function calculates the sum of two numbers.#+
+
+    Parameters:#+
+        num1 (int or float): The first number to be added.#+
+        num2 (int or float): The second number to be added.#+
+
+    Returns:#+
+        int or float: The sum of num1 and num2.#+
+    """#+
+    return num1 + num2
